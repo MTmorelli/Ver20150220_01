f2cc75a81d4499939aedb419326dbeefcd8fd91b
Mattia Morelli <m.mattiamorelli@gmail.com>
2015-02-20 09:58:50 +0100

    non sono riuscito a calcolare i maggiorenni... devo ripassare

--- main.c
+++ main.c
@@ -2,7 +2,29 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
+    int const N=5;
+    int i;
+    int array[N];
+    
+    printf("Inserisci l'età della prima persona\n");
+    scanf("%d", &N);
+    printf("Inserisci l'età della seconda persona\n");
+    scanf("%d", &N);
+    printf("Inserisci l'età della terza persona\n");
+    scanf("%d", &N);
+    printf("Inserisci l'età della quarta persona\n");
+    scanf("%d", &N);
+    printf("Inserisci l'età della quinta persona\n");
+    scanf("%d", &N);
+    
+    i = 0;
+    
+    while(N<18) {
+     
+    
+    
+    printf("I maggiorenni sono\n %d:", i);
+    }   
+        return (EXIT_SUCCESS);
 }
 
